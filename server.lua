5bf1e141319bb87cc6b470e81dca10778805e118
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-01-13 22:53:28 +0100

    tentative BDD mais marche po

--- server.lua
+++ server.lua
@@ -2,13 +2,28 @@ ESX = exports['es_extended']:getSharedObject()
 
 local DNA_ITEM = "dna_sample"
 local DNA_KIT = "dna_kit"
+local MySQL = exports.oxmysql
 
+local function collectDNA(ped_id, dnaId)
+    dnaId = tostring(dnaId)
+
+    -- Déclare la requête SQL
+    local query = 'INSERT INTO `dna_samples` (dnaId, ped_id) VALUES (?, ?)', { dnaId, ped_id }
+
+    -- Exécute la requête SQL
+    if query then
+        MySQL.query(query)
+    else
+        print("Error: query is undefined")
+    end
+end
 local function generateDNAId()
-    return "DNA-" .. math.random(100000, 999999)
+    return math.random(100000, 999999)
 end
 
 RegisterNetEvent('dna_collection:NPCMOW')
 AddEventHandler('dna_collection:NPCMOW', function(npcNetId)
+    collectDNA(npcNetId, generateDNAId())
     local xPlayer = ESX.GetPlayerFromId(source)
     if not xPlayer then return end
 
